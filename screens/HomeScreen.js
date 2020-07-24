889d1c234a685bd8cc540109c41ac0b5e834270e
Matas Lauzadis <matas.lauzadis@gmail.com>
2020-07-23 23:08:01 -0500

    convert HomeScreen to Component

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,22 +1,22 @@
 /* eslint-disable react/prop-types */
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, { Component } from 'react';
 import {Text, View, StyleSheet, TouchableOpacity} from 'react-native';
 
-const HomeScreen = ({navigation}) => {
-  return (
-    <View style={styles.container}>
-			<TouchableOpacity style={styles.button} onPress={ () => navigation.navigate('Send') }>
-        <Text style={{color: 'rgb(255,255,255)'}}>Send</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Receive')}>
-        <Text style={{color: 'rgb(255,255,255)'}}>Receive</Text>
-      </TouchableOpacity>
-    </View>
-  );
-};
-
-export default HomeScreen;
+class HomeScreen extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <TouchableOpacity style={styles.button} onPress={ () => this.props.navigation.navigate('Send') }>
+          <Text style={{color: 'rgb(255,255,255)'}}>Send</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => this.props.navigation.navigate('Receive')}>
+          <Text style={{color: 'rgb(255,255,255)'}}>Receive</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -37,3 +37,5 @@ const styles = StyleSheet.create({
     marginTop: 25,
   },
 });
+
+export default HomeScreen;
